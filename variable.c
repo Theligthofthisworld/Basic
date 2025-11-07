1aeeaa8ec2d2b045878820f7c4d5b0a8bd3e4cbb
Nigga <abejoe258@gmail.com>
2025-11-07 17:07:32 +0100

    Made some discovery about hashmap

--- variable.c
+++ variable.c
@@ -49,6 +49,14 @@ struct Variable CREATE_CHAR(char *value,char *name){
     return new;
 }
 
+int Var_free(void *item){
+    struct Variable *v=item;
+    free(v->name);
+    if(v->type==TYPE_STRING){
+        free(v->value.s);
+    }
+}
+
 struct Variable CREATE_BOOL(int value,char *name){
     struct Variable new;
     new.value.b=value;
@@ -68,8 +76,8 @@ uint64_t Variable_hash(const void *item, uint64_t seed0, uint64_t seed1) {
 }
 
 int main(void){
-    struct Variable i=CREATE_INTEGER(45,"mam");
-    struct Variable in=CREATE_INTEGER(45,"mama");
+    struct Variable i=CREATE_INTEGER(46,"mam");
+    struct Variable in=CREATE_INTEGER(45,"mam");
 
     struct hashmap *map=hashmap_new(sizeof(struct Variable),0,0,0,Variable_hash,Variable_compare,NULL,NULL);
     hashmap_set(map,&i);
@@ -77,6 +85,7 @@ int main(void){
     char name[4]="mam";
     struct Variable *v = hashmap_get(map, &(struct Variable){ .name = "mam" });
     printf("%s  are %d",v->name,v->value.i);
+    hashmap_free(map);
     return 0;
 
 
